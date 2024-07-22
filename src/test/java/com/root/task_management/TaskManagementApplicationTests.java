fa9b9e96fbdef12b014ee007fef1814f24ebcf93
swapno7064 <swapno7064@gmail.com>
2024-07-23 01:07:36 +0530

    Starter files, add dependencies and setup mongodb connection

---
+++ src/test/java/com/root/task_management/TaskManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.root.task_management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
